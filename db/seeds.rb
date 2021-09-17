a3571dbb21856eca04e0300036fb0aa3574edd95
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-17 15:58:05 -0500

    fix(seeds): require faker and refresh translations

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'faker'
+I18n.reload!
 
 def fake_markdown_body
   body = []

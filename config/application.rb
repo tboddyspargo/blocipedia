87c66cef263d60cbb4249a323bcebc84948e81c9
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-17 13:07:16 -0500

    fix(): avoid throwing errors on migrations pending before they have had a chance to run

--- config/application.rb
+++ config/application.rb
@@ -21,16 +21,8 @@ module Blocipedia
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-    
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
-      class_attr_index = html_tag.index 'class="'
+    config.active_record.raise_in_transactional_callbacks = false
 
-      if class_attr_index
-        html_tag.insert class_attr_index+7, 'is-invalid '
-      else
-        html_tag.insert html_tag.index('>'), ' class="is-invalid"'
-      end
-    }
+    
   end
 end

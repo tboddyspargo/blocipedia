c48655373f4e2fe40dcf6beb9614ebb565599641
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-17 19:33:23 -0500

    test(): move fake wiki generation to module for better encapsulation; fix tests

--- spec/factories/wikis.rb
+++ spec/factories/wikis.rb
@@ -1,20 +1,9 @@
-
-def fake_markdown_body
-  body = []
-  5.times.with_index do |i|
-    body << "## #{Faker::Lorem.words(4,true).join(' ')}" << "\n"
-    body << Faker::Lorem.paragraph << "\n"
-    body << Faker::Markdown.random << "\n"
-    body << Faker::Lorem.paragraph << "\n"
-    body << Faker::Markdown.random
-  end
-  body.join("\n")
-end
+require 'wiki_faker'
 
 FactoryBot.define do
   factory :wiki do
     title { Faker::Lorem.sentence.titlecase }
-    body { fake_markdown_body }
+    body { WikiFaker::markdown_body }
     
     trait :with_owner do
       transient do
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -8,10 +8,9 @@ RSpec.describe Wiki, type: :model do
   context "validations" do
     it { should validate_presence_of(:owner) }
     
-    it { should validate_length_of(:title).is_at_least(8) }
+    it { should validate_length_of(:title).is_at_least(1) }
     it { should validate_length_of(:title).is_at_most(140) }
     
-    it { should validate_length_of(:body).is_at_least(8) }
     it { should validate_length_of(:body).is_at_most(20000) }
     
     it { should allow_value(Faker::Boolean.boolean).for(:private) }

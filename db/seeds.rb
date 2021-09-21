db37d90b0e9539ee220e485b9c0050f3287ea6f7
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-21 17:43:15 -0500

    test: use vscode debugger with additional gems for testing; reformat and modernize conventions for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -8,43 +10,43 @@
 require 'wiki_faker'
 
 admin = User.new({
-  email: 'admin@blocipedia.com',
-  role: 'admin',
-  first_name: 'Jared',
-  last_name: 'Brokowski',
-  password: 'p@ssw0rd',
-  password_confirmation: 'p@ssw0rd',
-  confirmed_at: Date.today
-})
+                   email: 'admin@blocipedia.com',
+                   role: 'admin',
+                   first_name: 'Jared',
+                   last_name: 'Brokowski',
+                   password: 'p@ssw0rd',
+                   password_confirmation: 'p@ssw0rd',
+                   confirmed_at: Date.today
+                 })
 admin.skip_confirmation_notification!
 admin.save!
 
 premium = User.new({
-  email: 'member@blocipedia.com',
-  password: 'p@ssw0rd',
-  role: 'premium',
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  password_confirmation: 'p@ssw0rd',
-  confirmed_at: Date.today
-})
+                     email: 'member@blocipedia.com',
+                     password: 'p@ssw0rd',
+                     role: 'premium',
+                     first_name: Faker::Name.first_name,
+                     last_name: Faker::Name.last_name,
+                     password_confirmation: 'p@ssw0rd',
+                     confirmed_at: Date.today
+                   })
 premium.skip_confirmation_notification!
 premium.save!
 
 standard = User.new({
-  email: 'user@blocipedia.com',
-  password: 'p@ssw0rd',
-  role: 'standard',
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  password_confirmation: 'p@ssw0rd',
-  confirmed_at: Date.today
-})
+                      email: 'user@blocipedia.com',
+                      password: 'p@ssw0rd',
+                      role: 'standard',
+                      first_name: Faker::Name.first_name,
+                      last_name: Faker::Name.last_name,
+                      password_confirmation: 'p@ssw0rd',
+                      confirmed_at: Date.today
+                    })
 standard.skip_confirmation_notification!
 standard.save!
 
 rand(10..20).times do
-  user = User.create!(
+  User.create!(
     email: Faker::Internet.email,
     role: User.roles.keys.map { |k| k.to_s if k.to_s != 'admin' }.sample,
     first_name: Faker::Name.first_name,
@@ -59,7 +61,7 @@ users = User.all
 
 users.each do |u|
   rand(2..6).times do
-    WikiFaker::wiki_for_user(u).update_attribute(:created_at, Faker::Date.in_date_period)
+    WikiFaker.wiki_for_user(u).update_attribute(:created_at, Faker::Date.in_date_period)
   end
 end
 
